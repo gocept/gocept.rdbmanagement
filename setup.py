b58f3feb2fd634d4e0b75ac3f73bb00e79bca068
mac <mac@14c79854-3af7-0310-aa0e-852fec42f1e4>
2015-07-06 11:34:36 +0000

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ name = 'gocept.rdbmanagement'
 
 setup(
     name=name,
-    version='1.0',
+    version='1.1.dev0',
     author='Michael Howitz',
     author_email='mail@gocept.com',
     url='http://pypi.python.org/pypi/' + name,
